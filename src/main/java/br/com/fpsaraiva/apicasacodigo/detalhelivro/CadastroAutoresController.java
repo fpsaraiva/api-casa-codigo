d4fb6c1cf19548d98659c58439da40a3e3494c63
Fernando Saraiva <fpsaraiva@gmail.com>
2021-05-10 13:20:29 -0300

    finalizado cadastro de autor no banco

--- src/main/java/br/com/fpsaraiva/apicasacodigo/detalhelivro/CadastroAutoresController.java
+++ src/main/java/br/com/fpsaraiva/apicasacodigo/detalhelivro/CadastroAutoresController.java
@@ -1,5 +1,25 @@
 package br.com.fpsaraiva.apicasacodigo.detalhelivro;
 
-public class CadastroAutoresController {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CadastroAutoresController {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	@PostMapping(value="/api/autor")
+	@Transactional
+	public void novo(@Valid @RequestBody NovoAutorForm form) {
+		Autor novoAutor = form.novoAutor();
+		//System.out.println(novoAutor);
+		manager.persist(novoAutor);
+	}
 }
